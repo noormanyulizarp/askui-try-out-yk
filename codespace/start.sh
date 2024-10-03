044665ec66881f4eac2888952077fd2adb8a4ed4
Noorman Yulizar Prabowo <noormanyulizarp@gmail.com>
2024-10-03 05:05:10 +0000

    [start-sh] setting up the desktop workspace

--- codespace/start.sh
+++ codespace/start.sh
@@ -5,13 +5,13 @@ if [[ $DEBUG == "true" ]]; then
     set -xv
 fi
 
-# Run the VNC setup script in the background
-echo "Starting noVNC..."
-./start-novnc.sh &  # Run noVNC in the background
-
 # Mount cloud storage
 echo "Mounting cloud storage..."
-./mount-cloud-storage.sh
+./mount-cloud-storage.sh &
+
+# Run the VNC setup script in the background
+echo "Starting noVNC..."
+./start-novnc.sh   # Run noVNC in the background
 
 # Starting Zsh shell interactively with proper environment setup
 echo "Starting Zsh shell..."

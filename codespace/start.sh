b5af366b1ff68efe1793edd96503543b4cfff11f
Noorman Yulizar Prabowo <noormanyulizarp@gmail.com>
2024-10-03 02:49:13 +0000

    [start] fixing start sh script

--- codespace/start.sh
+++ codespace/start.sh
@@ -5,6 +5,14 @@ if [[ $DEBUG == "true" ]]; then
     set -xv
 fi
 
+# Run the VNC setup script in the background
+echo "Starting noVNC..."
+./start-novnc.sh &  # Run noVNC in the background
+
+# Mount cloud storage
+echo "Mounting cloud storage..."
+./mount-cloud-storage.sh
+
 # Starting Zsh shell interactively with proper environment setup
 echo "Starting Zsh shell..."
 exec zsh -l
